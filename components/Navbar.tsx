11279f781c25bc2a23e7ef4c19e50670f385ffb8
zidanhafiz <nadizsonata@gmail.com>
2024-02-28 19:23:54 +0800

    Fix account btn width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { logOut } from '@/lib/firebase/authLogin';
 import { useSignIn } from '@/context/SignInProvider';
+import { ThemeToggle } from './ThemeToggle';
 
 const Navbar = () => {
   return (
@@ -24,7 +25,10 @@ const Navbar = () => {
         >
           Cashier App
         </Link>
-        <AccountBtn />
+        <div className='flex items-center gap-4 w-fit'>
+          <ThemeToggle />
+          <AccountBtn />
+        </div>
       </nav>
     </header>
   );
@@ -34,28 +38,28 @@ const AccountBtn = () => {
   const router = useRouter();
   const { user } = useSignIn();
 
-  console.log(user);
-
   const goToSetting = () => {
     router.push('/settings');
   };
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className='flex items-center gap-3 py-3 px-5 border rounded-full'>
-        <div className='flex flex-col gap-0 items-end'>
-          <span className='text-base font-semibold'>{user?.email}</span>
+      <DropdownMenuTrigger className='grid grid-cols-[1fr,40px] justify-items-end gap-2 py-2 w-fit px-4 border rounded-full'>
+        <div className='flex flex-col gap-0 items-end w-full max-w-[100px]'>
+          <span className='text-base font-semibold w-full truncate'>
+            {user?.displayName}
+          </span>
           <span className='text-xs block text-gray-400'>Admin</span>
         </div>
         <Avatar>
-          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarImage src={user?.photoURL || ''} />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={goToSetting}>Profile</DropdownMenuItem>
+        <DropdownMenuItem onClick={goToSetting}>Settings</DropdownMenuItem>
         <DropdownMenuItem
           className='text-red-400'
           onClick={logOut}
