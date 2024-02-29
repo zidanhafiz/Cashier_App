cd7dadf4095edcca0bf1e6bc1258a065218e9403
zidanhafiz <nadizsonata@gmail.com>
2024-02-29 21:13:55 +0800

    Add back button component

---
+++ components/BackButton.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import { ChevronLeft } from 'lucide-react';
+import Link from 'next/link';
+import { ReactNode } from 'react';
+
+const BackButton = ({
+  children,
+  link,
+  className,
+}: {
+  children: ReactNode;
+  link: string;
+  className?: string;
+}) => {
+  return (
+    <Link
+      href={link}
+      className={cn(className, 'flex gap-2 text-base items-center text-slate-700')}
+    >
+      <ChevronLeft /> {children}
+    </Link>
+  );
+};
+
+export default BackButton;
