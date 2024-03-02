2659e58b0314d486fa53410b618beba7f71af7fb
zidanhafiz <nadizsonata@gmail.com>
2024-03-02 21:02:46 +0800

    Add category form field for create product page

--- app/(admin)/products/create/page.tsx
+++ app/(admin)/products/create/page.tsx
@@ -28,11 +28,23 @@ import { Textarea } from '@/components/ui/textarea';
 import { Label } from '@/components/ui/label';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import { createProduct, uploadTempImage } from '@/lib/firebase/productService';
+import {
+  createProduct,
+  getAllProducts,
+  uploadTempImage,
+} from '@/lib/firebase/productService';
 import { showAlert } from '@/components/Alert';
 import { useAlert } from '@/context/AlertProvider';
 import { showNotify } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
 
 const addAlertMessage = {
   title: 'Add Product',
@@ -52,12 +64,16 @@ const formSchema = z.object({
   image: z.instanceof(File),
   name: z.string().min(3).max(30),
   description: z.string().min(3).max(100),
+  category: z.string().min(3),
   price: z.number().min(1),
   stock: z.number().min(0),
 });
 
 const Create = () => {
   const [imgUrl, setImgUrl] = useState<string>('/product.png');
+  const [newCategory, setNewCategory] = useState<boolean>(false);
+  const [categories, setCategories] = useState<string[]>([]);
+
   const { showAlertHandle } = useAlert();
   const router = useRouter();
 
@@ -66,6 +82,7 @@ const Create = () => {
     defaultValues: {
       name: '',
       description: '',
+      category: '',
       price: 0,
       stock: 0,
       image: undefined,
@@ -100,6 +117,15 @@ const Create = () => {
   };
 
   useEffect(() => {
+    getAllProducts()
+      .then((res) => {
+        const data = res.map((cat) => cat.category);
+        setCategories(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
     if (imgWatch !== undefined) {
       uploadTempImage(imgWatch)
         .then((url) => setImgUrl(url))
@@ -108,7 +134,7 @@ const Create = () => {
   }, [imgWatch]);
 
   return (
-    <Card className='max-w-4xl'>
+    <Card className=''>
       <CardHeader>
         <BackButton
           link='/products'
@@ -120,17 +146,17 @@ const Create = () => {
         <CardDescription>Create your new product here.</CardDescription>
       </CardHeader>
       <Separator />
-      <CardContent className='mt-4'>
+      <CardContent className='mt-4 '>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className='space-y-4'
+            className='grid md:grid-cols-2 md:gap-8'
           >
             <FormField
               control={form.control}
               name='image'
               render={({ field: { value, onChange, ...field } }) => (
-                <FormItem>
+                <FormItem className='md:col-start-1'>
                   <Label>Preview</Label>
                   <Image
                     src={imgUrl}
@@ -156,94 +182,147 @@ const Create = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name='name'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder='Your product name'
-                      disabled={form.formState.isSubmitting}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='description'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder='Your product description'
-                      className='resize-none'
-                      disabled={form.formState.isSubmitting}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='price'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Price</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='number'
-                      placeholder='Your product price'
-                      {...field}
-                      onChange={(e) => field.onChange(parseInt(e.target.value))}
-                      disabled={form.formState.isSubmitting}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='stock'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Stock</FormLabel>
-                  <FormControl>
-                    <Input
-                      type='number'
-                      placeholder='Your product stock'
-                      {...field}
-                      onChange={(e) => field.onChange(parseInt(e.target.value))}
-                      disabled={form.formState.isSubmitting}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className='flex gap-3 pt-6 justify-end'>
-              <Button
-                type='reset'
-                className='bg-red-500 hover:bg-red-600 text-white'
-                onClick={resetHandler}
-                disabled={form.formState.isSubmitting}
-              >
-                Discard
-              </Button>
-              <Button
-                type='submit'
-                disabled={form.formState.isSubmitting}
-              >
-                Add Product
-              </Button>
+            <div className='md:col-start-2 space-y-4'>
+              <FormField
+                control={form.control}
+                name='name'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Product Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder='Your product name'
+                        disabled={form.formState.isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='description'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder='Your product description'
+                        className='resize-none'
+                        disabled={form.formState.isSubmitting}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div>
+                <FormLabel>Category</FormLabel>
+                <FormField
+                  control={form.control}
+                  name='category'
+                  render={({ field }) => (
+                    <FormItem className='pt-2 flex gap-8'>
+                      <FormControl>
+                        {newCategory ? (
+                          <Input
+                            className='w-1/2 max-w-[500px]'
+                            placeholder='Your product category'
+                            disabled={form.formState.isSubmitting}
+                            {...field}
+                          />
+                        ) : (
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <FormControl className='w-1/2 max-w-[500px]'>
+                              <SelectTrigger>
+                                <SelectValue placeholder='Select category' />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              {categories.map((cat) => (
+                                <SelectItem
+                                  key={cat}
+                                  value={cat}
+                                >
+                                  {cat}
+                                </SelectItem>
+                              ))}
+                            </SelectContent>
+                          </Select>
+                        )}
+                      </FormControl>
+                      <FormMessage />
+                      <div className='flex items-center gap-2'>
+                        <Switch
+                          id='create-category'
+                          checked={newCategory}
+                          onCheckedChange={() => setNewCategory(!newCategory)}
+                        />
+                        <Label htmlFor='create-category'>Create new category</Label>
+                      </div>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <FormField
+                control={form.control}
+                name='price'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='number'
+                        placeholder='Your product price'
+                        {...field}
+                        onChange={(e) => field.onChange(parseInt(e.target.value))}
+                        disabled={form.formState.isSubmitting}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='stock'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Product Stock</FormLabel>
+                    <FormControl>
+                      <Input
+                        type='number'
+                        placeholder='Your product stock'
+                        {...field}
+                        onChange={(e) => field.onChange(parseInt(e.target.value))}
+                        disabled={form.formState.isSubmitting}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className='flex gap-3 pt-6 justify-end'>
+                <Button
+                  type='reset'
+                  className='bg-red-500 hover:bg-red-600 text-white'
+                  onClick={resetHandler}
+                  disabled={form.formState.isSubmitting}
+                >
+                  Discard
+                </Button>
+                <Button
+                  type='submit'
+                  disabled={form.formState.isSubmitting}
+                >
+                  Add Product
+                </Button>
+              </div>
             </div>
           </form>
         </Form>
