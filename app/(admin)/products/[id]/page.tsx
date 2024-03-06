b2def485884dbd41cace6890d1499c539e6e3a37
zidan hafiz <nadizsonata@gmail.com>
2024-03-06 01:28:50 +0000

    Add scroll bar for form component in edit and create page

--- app/(admin)/products/[id]/page.tsx
+++ app/(admin)/products/[id]/page.tsx
@@ -48,6 +48,7 @@ import {
 } from '@/components/ui/select';
 import { Switch } from '@/components/ui/switch';
 import { DocumentData } from 'firebase/firestore';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const updateAlertMessage = {
   title: 'Update Product',
@@ -117,7 +118,8 @@ const Edit = () => {
   const resetHandler = async () => {
     showAlert(discardAlertMessage, 'discard', showAlertHandle).then(() => {
       Object.keys(product).forEach((fieldName: any) => {
-        if (fieldName !== 'image') return setValue(fieldName, product[fieldName]);
+        if (fieldName !== 'image')
+          return setValue(fieldName, product[fieldName]);
       });
       setImgUrl(product.image);
     });
@@ -155,198 +157,199 @@ const Edit = () => {
       <CardHeader>
         <BackButton
           link='/products'
-          className='mb-3'
-        >
+          className='mb-3'>
           Back to products
         </BackButton>
         <CardTitle>Update Product</CardTitle>
         <CardDescription>Update your current product here.</CardDescription>
       </CardHeader>
       <Separator />
-      <CardContent className='mt-4 '>
-        <Form
-          setValue={setValue}
-          {...form}
-        >
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className='grid md:grid-cols-2 md:gap-8'
-          >
-            <FormField
-              control={form.control}
-              name='image'
-              render={({ field: { value, onChange, ...field } }) => (
-                <FormItem className='md:col-start-1'>
-                  <Label>Preview</Label>
-                  <Image
-                    src={imgUrl}
-                    width={200}
-                    height={200}
-                    className='w-[150px] h-[150px] object-cover mx-auto rounded-xl overflow-hidden shadow-lg'
-                    alt='profile image'
-                  />
-                  <FormLabel>Product Image</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder='avatar'
-                      type='file'
-                      accept='image/png,image/jpeg'
-                      onChange={(event) => {
-                        event.target.files && onChange(event.target.files[0]);
-                      }}
-                      disabled={form.formState.isSubmitting}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className='md:col-start-2 space-y-4'>
+      <ScrollArea className='h-[600px] md:h-auto'>
+        <CardContent className='mt-4 '>
+          <Form
+            setValue={setValue}
+            {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className='grid md:grid-cols-2 md:gap-8'>
               <FormField
                 control={form.control}
-                name='name'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Product Name</FormLabel>
+                name='image'
+                render={({ field: { value, onChange, ...field } }) => (
+                  <FormItem className='md:col-start-1'>
+                    <Label>Preview</Label>
+                    <Image
+                      src={imgUrl}
+                      width={200}
+                      height={200}
+                      className='w-[150px] h-[150px] object-cover mx-auto rounded-xl overflow-hidden shadow-lg'
+                      alt='profile image'
+                    />
+                    <FormLabel>Product Image</FormLabel>
                     <FormControl>
                       <Input
-                        placeholder='Your product name'
-                        disabled={form.formState.isSubmitting}
                         {...field}
+                        placeholder='avatar'
+                        type='file'
+                        accept='image/png,image/jpeg'
+                        onChange={(event) => {
+                          event.target.files && onChange(event.target.files[0]);
+                        }}
+                        disabled={form.formState.isSubmitting}
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <FormField
-                control={form.control}
-                name='description'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Description</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        placeholder='Your product description'
-                        className='resize-none'
-                        disabled={form.formState.isSubmitting}
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div>
-                <FormLabel>Category</FormLabel>
+              <div className='md:col-start-2 space-y-4'>
                 <FormField
                   control={form.control}
-                  name='category'
+                  name='name'
                   render={({ field }) => (
-                    <FormItem className='pt-2 flex gap-8'>
+                    <FormItem>
+                      <FormLabel>Product Name</FormLabel>
                       <FormControl>
-                        {newCategory ? (
-                          <Input
-                            className='w-1/2 max-w-[500px]'
-                            placeholder='Your product category'
-                            disabled={form.formState.isSubmitting}
-                            {...field}
-                          />
-                        ) : (
-                          <Select
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                          >
-                            <FormControl className='w-1/2 max-w-[500px]'>
-                              <SelectTrigger>
-                                <SelectValue placeholder='Select category' />
-                              </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                              {categories.map((cat) => (
-                                <SelectItem
-                                  key={cat}
-                                  value={cat}
-                                >
-                                  {capitalizeFirstWord(cat)}
-                                </SelectItem>
-                              ))}
-                            </SelectContent>
-                          </Select>
-                        )}
+                        <Input
+                          placeholder='Your product name'
+                          disabled={form.formState.isSubmitting}
+                          {...field}
+                        />
                       </FormControl>
                       <FormMessage />
-                      <div className='flex items-center gap-2'>
-                        <Switch
-                          id='create-category'
-                          checked={newCategory}
-                          onCheckedChange={() => setNewCategory(!newCategory)}
-                        />
-                        <Label htmlFor='create-category'>Create new category</Label>
-                      </div>
                     </FormItem>
                   )}
                 />
+                <FormField
+                  control={form.control}
+                  name='description'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea
+                          placeholder='Your product description'
+                          className='resize-none'
+                          disabled={form.formState.isSubmitting}
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div>
+                  <FormLabel>Category</FormLabel>
+                  <FormField
+                    control={form.control}
+                    name='category'
+                    render={({ field }) => (
+                      <FormItem className='pt-2 flex gap-8'>
+                        <FormControl>
+                          {newCategory ? (
+                            <Input
+                              className='w-1/2 max-w-[500px]'
+                              placeholder='Your product category'
+                              disabled={form.formState.isSubmitting}
+                              {...field}
+                            />
+                          ) : (
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}>
+                              <FormControl className='w-1/2 max-w-[500px]'>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Select category' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {categories.map((cat) => (
+                                  <SelectItem
+                                    key={cat}
+                                    value={cat}>
+                                    {capitalizeFirstWord(cat)}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </Select>
+                          )}
+                        </FormControl>
+                        <FormMessage />
+                        <div className='flex items-center gap-2'>
+                          <Switch
+                            id='create-category'
+                            checked={newCategory}
+                            onCheckedChange={() => setNewCategory(!newCategory)}
+                          />
+                          <Label htmlFor='create-category'>
+                            Create new category
+                          </Label>
+                        </div>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name='price'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Price</FormLabel>
+                      <FormControl>
+                        <Input
+                          type='number'
+                          placeholder='Your product price'
+                          {...field}
+                          onChange={(e) =>
+                            field.onChange(parseInt(e.target.value))
+                          }
+                          disabled={form.formState.isSubmitting}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name='stock'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Product Stock</FormLabel>
+                      <FormControl>
+                        <Input
+                          type='number'
+                          placeholder='Your product stock'
+                          {...field}
+                          onChange={(e) =>
+                            field.onChange(parseInt(e.target.value))
+                          }
+                          disabled={form.formState.isSubmitting}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className='flex gap-3 pt-6 justify-end'>
+                  <Button
+                    type='reset'
+                    className='bg-red-500 hover:bg-red-600 text-white'
+                    onClick={resetHandler}
+                    disabled={form.formState.isSubmitting}>
+                    Discard
+                  </Button>
+                  <Button
+                    type='submit'
+                    disabled={form.formState.isSubmitting}>
+                    Update Product
+                  </Button>
+                </div>
               </div>
-              <FormField
-                control={form.control}
-                name='price'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Price</FormLabel>
-                    <FormControl>
-                      <Input
-                        type='number'
-                        placeholder='Your product price'
-                        {...field}
-                        onChange={(e) => field.onChange(parseInt(e.target.value))}
-                        disabled={form.formState.isSubmitting}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name='stock'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Product Stock</FormLabel>
-                    <FormControl>
-                      <Input
-                        type='number'
-                        placeholder='Your product stock'
-                        {...field}
-                        onChange={(e) => field.onChange(parseInt(e.target.value))}
-                        disabled={form.formState.isSubmitting}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <div className='flex gap-3 pt-6 justify-end'>
-                <Button
-                  type='reset'
-                  className='bg-red-500 hover:bg-red-600 text-white'
-                  onClick={resetHandler}
-                  disabled={form.formState.isSubmitting}
-                >
-                  Discard
-                </Button>
-                <Button
-                  type='submit'
-                  disabled={form.formState.isSubmitting}
-                >
-                  Update Product
-                </Button>
-              </div>
-            </div>
-          </form>
-        </Form>
-      </CardContent>
+            </form>
+          </Form>
+        </CardContent>
+      </ScrollArea>
     </Card>
   );
 };
