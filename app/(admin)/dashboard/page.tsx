c03936ae9eadab9625b7e7cf44a68aed24b616bd
zidanhafiz <nadizsonata@gmail.com>
2024-03-07 09:09:17 +0800

    Remove unused component

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -4,7 +4,6 @@ import DialogModal from '@/components/DialogModal';
 import ProductCard from '@/components/ProductCard';
 import SearchForm from '@/components/SearchForm';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { getAllProducts } from '@/lib/firebase/productService';
 import { Product } from '@/types';
